5921af5dfe07175697b58f6ebf108f4d9446f1b9
Dmitry Chigrin <chigrin.dmitry@gmail.com>
2023-01-01 17:34:57 +0500

    feat: add assert tests

--- hexlet-tests/tests/capitalize.test.js
+++ hexlet-tests/tests/capitalize.test.js
@@ -1,3 +1,5 @@
+import { strict as assert } from 'node:assert';
+// import chai from 'chai';
 import capitalize from '../src/capitalize.js';
 
 if (capitalize('hello') !== 'Hello') {
@@ -9,3 +11,11 @@ if (capitalize('') !== '') {
 }
 
 console.log('Все тесты пройдены');
+
+assert(capitalize('hello') === 'Hello');
+assert.equal(capitalize('hello'), 'Hello');
+assert.equal(capitalize(''), '');
+
+// assert.deepEqual({}, {});
+// assert.deepEqual({ key: value }, { key: value });
+// assert.deepEqual({ key: value }, { key: anotherValue });
